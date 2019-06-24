aa00810be1c036261629ed6758d88d24e8923dbb
Guilherme Lima <gui.lima2304@hotmail.com>
2019-06-24 20:56:04 -0300

    add database with categories

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,15 @@
+import { InMemoryObService, httpClientInMemBackendServiceFactory } from "angular-in-memory-web-api";
+
+export class InMemoryDatabase implements InMemoryObService{
+    createDb(){
+        const categories = [
+            { id: 1, name: "Moradia", description: "Pagamentos de contas da casa" },
+            { id: 2, name: "Saúde", description: "Planos de saúde e remédio" },
+            { id: 3, name: "Lazer", description: "Cinemas, praias, parques, etc" },
+            { id: 4, name: "Salário", description: "Recebimentos de Salários" },
+            { id: 5, name: "Freelas", description: "Trabalhos como Freelancer" }
+        ];
+
+        return {categories}
+    }
+}
